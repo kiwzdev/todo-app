c60acc9bc38e3d74f3ec9f4225f3aca13318976e
kiwz <kiwzdev@gmail.com>
2025-06-12 12:23:04 +0700

    feat: add responsive loading component

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,7 +1,7 @@
 export default function Loading() {
   return (
     <div className="min-h-screen flex items-center justify-center">
-      <div className="w-20 h-20 border-4 border-gray-300 border-t-4 border-t-blue-400 rounded-full animate-spin"></div>
+      <div className="w-20 h-20 sm:w-24 sm:h-24 md:w-28 md:h-28 border-4 border-gray-300 border-t-blue-500 rounded-full animate-spin"></div>
     </div>
   );
 }
